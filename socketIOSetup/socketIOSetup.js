32388ed19c1f2e547b3b4bc47c93e9b2f5c6306c
Moshe Siegel <siegel.moshes@gmail.com>
2021-12-01 23:09:05 -0500

    add vercel deploy url to socket origins
    Adds vercel preview urls to list of valid CORs
    
    Test plan:
    I ran this backend branch locally, accessed the vercel preview url of https://frempco-git-setup-socketio-mssiegel.vercel.app and visited the teachers page and saw the socket id listed.

--- socketIOSetup/socketIOSetup.js
+++ socketIOSetup/socketIOSetup.js
@@ -1,9 +1,13 @@
 import { Server } from 'socket.io';
 
 export default function socketIOSetup(server) {
+  const PRODUCTION_URL = 'https://www.frempco.com';
+  const DEV_URL = 'http://localhost:3000';
+  const VERCEL_DEPLOY_PREVIEW_URL = /https:..frempco.*vercel.app/;
+
   const io = new Server(server, {
     cors: {
-      origin: ['https://www.frempco.com', 'http://localhost:3000'],
+      origin: [PRODUCTION_URL, DEV_URL, VERCEL_DEPLOY_PREVIEW_URL],
     },
   });
 
