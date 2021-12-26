07f92825bcaceb829b78290d73e4371940b25fa3
Moshe Siegel <siegel.moshes@gmail.com>
2021-12-25 22:31:20 -0500

    Teacher is notified when a new student enters
    - feat: Teacher is notified whenever a new student enters
    - refactor: replaced adding or deleting classroom from RESTful commands to socket events

---
+++ services/database.js
@@ -0,0 +1,34 @@
+export const classrooms = {}; // map classroomName => {teacherSocketId, students:[{name, socketId}]}
+export const teachers = {}; // map socket.id => {socket, classroomName}
+export const students = {}; // map socket.id => socket
+
+export function getClassroom(classroomName) {
+  return classrooms[classroomName];
+}
+
+export function addClassroom(classroomName, socket) {
+  teachers[socket.id] = { socket, classroomName };
+  classrooms[classroomName] = {
+    teacherSocketId: socket.id,
+    students: [],
+  };
+}
+
+export function deleteClassroom(classroomName, teacherSocketId) {
+  delete classrooms[classroomName];
+  delete teachers[teacherSocketId];
+}
+
+export function getTeacher(socketId) {
+  return teachers[socketId];
+}
+
+export function addStudentToClassroom(student, classroomName) {
+  // add student
+  const classroom = classrooms[classroomName];
+  classroom.students.push(student);
+
+  // inform teacher
+  const teacherSocket = teachers[classroom.teacherSocketId].socket;
+  teacherSocket.emit('new student joined', student.name);
+}
