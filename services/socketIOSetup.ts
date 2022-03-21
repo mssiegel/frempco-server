c38b2ef2d3cbb625f25a958f8431bda6d26e8bf8
Moshe Siegel <siegel.moshes@gmail.com>
2022-03-20 23:50:01 -0400

    Added the error catcher to every socket event listener

--- services/socketIOSetup.ts
+++ services/socketIOSetup.ts
@@ -1,7 +1,7 @@
 import { Server } from 'socket.io';
 
 import corsOptions from './corsOptions.js';
-
+import errorCatcher from '../utils/errorCatcher.js';
 import {
   addClassroom,
   deleteClassroom,
@@ -27,43 +27,58 @@ export default function socketIOSetup(server) {
 
       const student = getStudent(socket.id);
       if (student) remStudentFromClassroom(student);
-    }
-    socket.on('disconnect', userDisconnected);
-    socket.on('user disconnected', userDisconnected);
+    };
+    socket.on('disconnect', errorCatcher(userDisconnected));
+    socket.on('user disconnected', errorCatcher(userDisconnected));
 
-    socket.on('activate classroom', ({ classroomName }) => {
-      addClassroom(classroomName, socket);
-    });
+    socket.on(
+      'activate classroom',
+      errorCatcher(({ classroomName }) => {
+        addClassroom(classroomName, socket);
+      }),
+    );
 
-    socket.on('deactivate classroom', () => {
-      const teacher = getTeacher(socket.id);
-      if (teacher) deleteClassroom(teacher);
-    });
+    socket.on(
+      'deactivate classroom',
+      errorCatcher(() => {
+        const teacher = getTeacher(socket.id);
+        if (teacher) deleteClassroom(teacher);
+      }),
+    );
 
     socket.on(
       'new student entered',
-      ({ student: realName, classroom: classroomName }) => {
+      errorCatcher(({ student: realName, classroom: classroomName }) => {
         addStudentToClassroom(realName, classroomName, socket);
-      },
+      }),
     );
 
     // Teacher pairs up their students
-    socket.on('pair students', ({ studentPairs }) => {
-      pairStudents(studentPairs, socket);
-    });
+    socket.on(
+      'pair students',
+      errorCatcher(({ studentPairs }) => {
+        pairStudents(studentPairs, socket);
+      }),
+    );
 
-    socket.on('remove student from classroom', ({ socketId }) => {
-      const student = getStudent(socketId);
-      if (student) {
-        remStudentFromClassroom(student);
-        student.socket.emit('remove student from classroom')
-      }
-    });
+    socket.on(
+      'remove student from classroom',
+      errorCatcher(({ socketId }) => {
+        const student = getStudent(socketId);
+        if (student) {
+          remStudentFromClassroom(student);
+          student.socket.emit('remove student from classroom');
+        }
+      }),
+    );
 
-    socket.on('unpair student chat', ({ chatId, student1, student2 }) => {
-      const teacher = getTeacher(socket.id);
-      unpairStudentChat(teacher.socket, chatId, student1, student2)
-    });
+    socket.on(
+      'unpair student chat',
+      errorCatcher(({ chatId, student1, student2 }) => {
+        const teacher = getTeacher(socket.id);
+        unpairStudentChat(teacher.socket, chatId, student1, student2);
+      }),
+    );
 
     // New chat message sent from one student to their peer
     socket.on(
@@ -74,18 +89,11 @@ export default function socketIOSetup(server) {
     );
 
     // New chat message sent from one student to their peer
-    socket.on('student typing', ({ character }) => {
-      sendUserTyping(character, socket);
-    });
+    socket.on(
+      'student typing',
+      errorCatcher(({ character }) => {
+        sendUserTyping(character, socket);
+      }),
+    );
   });
-}
-
-function errorCatcher(originalFunction) {
-  return (...args) => {
-    try {
-      originalFunction(...args);
-    } catch (err) {
-      console.log(err);
-    }
-  };
 }
